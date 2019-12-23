ba6a5cbe849411cfb1279b5e3d0b539cee8c7c77
BeeSupper <lxbinh90@gmail.com>
2019-12-23 20:43:01 +0700

    Like and Category Migration

--- database/migrations/2019_12_23_133229_create_likes_table.php
+++ database/migrations/2019_12_23_133229_create_likes_table.php
@@ -15,6 +15,9 @@ class CreateLikesTable extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->bigIncrements('id');
+
+            $table->bigInteger('reply_id');
+            $table->bigInteger('user_id');
             $table->timestamps();
         });
     }
