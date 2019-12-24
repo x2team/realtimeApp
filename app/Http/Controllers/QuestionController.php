3aa1249859e0e07390d78a4f82752d39bb389a47
BeeSupper <lxbinh90@gmail.com>
2019-12-24 21:39:43 +0700

    CRUD for Category

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -75,7 +75,8 @@ class QuestionController extends Controller
      */
     public function update(Request $request, Question $question)
     {
-        //
+        $question->update($request->all());
+        return response('Updated', Response::HTTP_ACCEPTED);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ use Illuminate\Http\Request;
 // });
 
 Route::apiResource('/question', 'QuestionController');
+Route::apiResource('/category', 'CategoryController');
